234a748484ce9813ebb57402cea7d88a67595550
Lauris <la.stepanovs@gmail.com>
2021-08-08 21:06:07 +0300

    master: new build

--- client/src/api/api.ts
+++ client/src/api/api.ts
@@ -4,7 +4,7 @@ import axios from "axios";
 axios.defaults.headers.post["Content-Type"] = "application/json;charset=utf-8";
 axios.defaults.headers.post["Access-Control-Allow-Origin"] = "*";
 
-const apiLink = "/api";
+const apiLink = "http://46.101.172.144:8000/api";
 
 const BaseApi = axios.create({
   baseURL: apiLink
