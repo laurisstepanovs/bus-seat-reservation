9f978c35130fb70c3c9d17c82d931ac68ac4fbca
Lauris <la.stepanovs@gmail.com>
2021-08-08 15:51:41 +0300

    master: updated base url

--- client/src/api/api.ts
+++ client/src/api/api.ts
@@ -4,9 +4,7 @@ import axios from "axios";
 axios.defaults.headers.post["Content-Type"] = "application/json;charset=utf-8";
 axios.defaults.headers.post["Access-Control-Allow-Origin"] = "*";
 
-const apiLink = process.env.NODE_ENV === "production"
-    ? "http://46.101.172.144:8000/api"
-    : "http://127.0.0.1:8000/api"
+const apiLink = "http://46.101.172.144:8000/api";
 
 const BaseApi = axios.create({
   baseURL: apiLink
